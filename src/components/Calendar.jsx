88fddfe69d030a08acec85bf6c924c9cae6f563d
Eddie Haddican <edwardhaddican@gmail.com>
2021-07-06 14:20:31 -0400

    converted calendar to hook based

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -1,24 +1,14 @@
 import React from "react";
 
-class Calendar extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      events: [],
-    };
-  }
-
-  render() {
-    return (
-      <iframe
-        src="https://calendar.google.com/calendar/embed?src=carpathia.holdfast%40gmail.com&ctz=America%2FNew_York"
-        frameborder="0"
-        scrolling="no"
-        className='calendar'
-      ></iframe>
-    );
-
-  }
-}
+const Calendar = () => {
+  return (
+    <iframe
+      src="https://calendar.google.com/calendar/embed?src=carpathia.holdfast%40gmail.com&ctz=America%2FNew_York"
+      frameborder="0"
+      scrolling="no"
+      className="calendar"
+    ></iframe>
+  );
+};
 
 export default Calendar;
