a2ddf8cbf26ae1f78a8972360f8f0171b3800184
Eddie Haddican <edwardhaddican@gmail.com>
2021-07-06 14:07:18 -0400

    embedded calendar with ifram

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React from "react";
 
-const Calendar = ()=>{
-  return(
-    <div>
-      <h1>Calendar</h1>
-    </div>
-  )
+class Calendar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      events: [],
+    };
+  }
+
+  render() {
+    return (
+      <iframe
+        src="https://calendar.google.com/calendar/embed?src=carpathia.holdfast%40gmail.com&ctz=America%2FNew_York"
+        frameborder="0"
+        scrolling="no"
+        className='calendar'
+      ></iframe>
+    );
+
+  }
 }
 
-export default Calendar
+export default Calendar;
