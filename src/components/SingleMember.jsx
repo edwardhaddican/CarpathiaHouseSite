364fb907a2856f2ccf0d49af7ec8fd03e11b6543
Eddie Haddican <edwardhaddican@gmail.com>
2021-07-29 00:37:28 -0400

    working on adding an edit button to togle the single member page from a display to a form, still working on it

--- src/components/SingleMember.jsx
+++ src/components/SingleMember.jsx
@@ -4,6 +4,7 @@ import axios from "axios";
 const SingleMember = ({ member, match }) => {
   const [singleMemberData, setSingleMemberData] = useState(null);
   const [currentId, setCurrentId] = useState(null);
+  const [isEdit, setIsEdit] = useState(false);
 
   async function findSingleMember() {
     try {
@@ -23,123 +24,307 @@ const SingleMember = ({ member, match }) => {
     findSingleMember();
   }, []);
 
+  function editClickHandler(){
+    console.log('i am edit')
+  }
+
+  function saveClickHandler(){
+    console.log('i am save')
+  }
+
   return (
-    <div className="single-member-card">
-      {singleMemberData ? (
-        <img
-          src={singleMemberData.image}
-          alt="the members portrait"
-          className="single-member-image"
-        />
-      ) : null}
+    <>
+      {isEdit ? (
+        <div className="single-member-card">
+          {singleMemberData ? (
+            <img
+              src={singleMemberData.image}
+              alt="the members portrait"
+              className="single-member-image"
+            />
+          ) : null}
 
-      {singleMemberData ? (
-        <p>
-          <span className="single-member-label"> Mundane Name: </span>
-          {singleMemberData.mundaneLastName}, {singleMemberData.mundaneFirstName}{" "}
-        </p>
-      ) : null}
+          <div className="single-member-card-inner-container">
+            {singleMemberData ? (
+              <p>
+                <span className="single-member-label"> Mundane Name: </span>
+                {singleMemberData.mundaneLastName},{" "}
+                {singleMemberData.mundaneFirstName}{" "}
+              </p>
+            ) : null}
 
-      {singleMemberData ? (
-        <p>
-          <span className="single-member-label"> SCA Name: </span>
-          {singleMemberData.scaLastName}, {singleMemberData.scaFirstName}{" "}
-        </p>
-      ) : null}
+            {singleMemberData ? (
+              <p>
+                <span className="single-member-label"> SCA Name: </span>
+                {singleMemberData.scaLastName}, {singleMemberData.scaFirstName}{" "}
+              </p>
+            ) : null}
 
-      {singleMemberData ? (
-        <p>
-          <span className="single-member-label">
-            Is this member's name registered?:
-          </span>{" "}
-          {singleMemberData.registeredName ? <span>Yes</span> : <span>no</span>}{" "}
-        </p>
-      ) : null}
+            {singleMemberData ? (
+              <p>
+                <span className="single-member-label">
+                  Is this member's name registered?:
+                </span>{" "}
+                {singleMemberData.registeredName ? (
+                  <span>Yes</span>
+                ) : (
+                  <span>no</span>
+                )}{" "}
+              </p>
+            ) : null}
 
-      {singleMemberData ? (
-        <p>
-          <span className="single-member-label">
-            Is this member's arms registered?:
-          </span>{" "}
-          {singleMemberData.registeredArms ? <span>Yes</span> : <span>no</span>}{" "}
-        </p>
-      ) : null}
+            {singleMemberData ? (
+              <p>
+                <span className="single-member-label">
+                  Is this member's arms registered?:
+                </span>{" "}
+                {singleMemberData.registeredArms ? (
+                  <span>Yes</span>
+                ) : (
+                  <span>no</span>
+                )}{" "}
+              </p>
+            ) : null}
 
-      {singleMemberData ? (
-        <p>
-          <span className="single-member-label">Singles Fighter:</span>{" "}
-          {singleMemberData.singlesFighter ? <span>Yes</span> : <span>no</span>}{" "}
-        </p>
-      ) : null}
+            {singleMemberData ? (
+              <p>
+                <span className="single-member-label">Singles Fighter:</span>{" "}
+                {singleMemberData.singlesFighter ? (
+                  <span>Yes</span>
+                ) : (
+                  <span>no</span>
+                )}{" "}
+              </p>
+            ) : null}
 
-      {singleMemberData ? (
-        <p>
-          <span className="single-member-label">Melee Fighter: </span>{" "}
-          {singleMemberData.meleeFighter ? <span>Yes</span> : <span>no</span>}{" "}
-        </p>
-      ) : null}
+            {singleMemberData ? (
+              <p>
+                <span className="single-member-label">Melee Fighter: </span>{" "}
+                {singleMemberData.meleeFighter ? (
+                  <span>Yes</span>
+                ) : (
+                  <span>no</span>
+                )}{" "}
+              </p>
+            ) : null}
 
-      {singleMemberData ? (
-        <p>
-          <span className="single-member-label">Combat Archer: </span>{" "}
-          {singleMemberData.combatArcher ? <span>Yes</span> : <span>no</span>}{" "}
-        </p>
-      ) : null}
+            {singleMemberData ? (
+              <p>
+                <span className="single-member-label">Combat Archer: </span>{" "}
+                {singleMemberData.combatArcher ? (
+                  <span>Yes</span>
+                ) : (
+                  <span>no</span>
+                )}{" "}
+              </p>
+            ) : null}
 
-      {singleMemberData && singleMemberData.authorizedForms.length > 0 ? (
-        <p>
-          <span className="single-member-label">
-            Authorized Weapons Forms:{" "}
-          </span>{" "}
-          {singleMemberData.authorizedForms.join(", ")}
-        </p>
+            {singleMemberData && singleMemberData.authorizedForms.length > 0 ? (
+              <p>
+                <span className="single-member-label">
+                  Authorized Weapons Forms:{" "}
+                </span>{" "}
+                {singleMemberData.authorizedForms.join(", ")}
+              </p>
+            ) : (
+              <p>
+                <span className="single-member-label">
+                  Authorized Weapons Forms:{" "}
+                </span>{" "}
+                No weapons forms authorized
+              </p>
+            )}
+
+            {singleMemberData ? (
+              <p>
+                <span className="single-member-label">
+                  Is Gear ready for fighting:{" "}
+                </span>{" "}
+                {singleMemberData.gearStatus ? (
+                  <span>Yes</span>
+                ) : (
+                  <span>no</span>
+                )}{" "}
+              </p>
+            ) : null}
+
+            {singleMemberData ? (
+              <p>
+                <span className="single-member-label">Member:</span>{" "}
+                {singleMemberData.memberStatus}
+              </p>
+            ) : null}
+
+            {singleMemberData ? (
+              <p>
+                <span className="single-member-label">Is Admin: </span>{" "}
+                {singleMemberData.admin ? <span>Yes</span> : <span>no</span>}{" "}
+              </p>
+            ) : null}
+
+            {singleMemberData ? (
+              <p>
+                <span className="single-member-label">Rank: </span>
+                {singleMemberData.rank}
+              </p>
+            ) : null}
+          </div>
+          {singleMemberData ? (
+            <img
+              src={singleMemberData.arms}
+              alt="the members arms"
+              className="single-member-image"
+            />
+          ) : null}
+           <button onClick={saveClickHandler}>Save</button>
+        </div>
       ) : (
-        <p>
-          <span className="single-member-label">
-            Authorized Weapons Forms:{" "}
-          </span>{" "}
-          No weapons forms authorized
-        </p>
+        <div className="single-member-card">
+          {singleMemberData ? (
+            <img
+              src={singleMemberData.image}
+              alt="the members portrait"
+              className="single-member-image"
+            />
+          ) : null}
+
+          <div className="single-member-card-inner-container">
+            {singleMemberData ? (
+              <p>
+                <span className="single-member-label"> Mundane Name: </span>
+                {singleMemberData.mundaneLastName},{" "}
+                {singleMemberData.mundaneFirstName}{" "}
+              </p>
+            ) : null}
+
+            {singleMemberData ? (
+              <p>
+                <span className="single-member-label"> SCA Name: </span>
+                {singleMemberData.scaLastName}, {singleMemberData.scaFirstName}{" "}
+              </p>
+            ) : null}
+
+            {singleMemberData ? (
+              <p>
+                <span className="single-member-label">
+                  Is this member's name registered?:
+                </span>{" "}
+                {singleMemberData.registeredName ? (
+                  <span>Yes</span>
+                ) : (
+                  <span>no</span>
+                )}{" "}
+              </p>
+            ) : null}
+
+            {singleMemberData ? (
+              <p>
+                <span className="single-member-label">
+                  Is this member's arms registered?:
+                </span>{" "}
+                {singleMemberData.registeredArms ? (
+                  <span>Yes</span>
+                ) : (
+                  <span>no</span>
+                )}{" "}
+              </p>
+            ) : null}
+
+            {singleMemberData ? (
+              <p>
+                <span className="single-member-label">Singles Fighter:</span>{" "}
+                {singleMemberData.singlesFighter ? (
+                  <span>Yes</span>
+                ) : (
+                  <span>no</span>
+                )}{" "}
+              </p>
+            ) : null}
+
+            {singleMemberData ? (
+              <p>
+                <span className="single-member-label">Melee Fighter: </span>{" "}
+                {singleMemberData.meleeFighter ? (
+                  <span>Yes</span>
+                ) : (
+                  <span>no</span>
+                )}{" "}
+              </p>
+            ) : null}
+
+            {singleMemberData ? (
+              <p>
+                <span className="single-member-label">Combat Archer: </span>{" "}
+                {singleMemberData.combatArcher ? (
+                  <span>Yes</span>
+                ) : (
+                  <span>no</span>
+                )}{" "}
+              </p>
+            ) : null}
+
+            {singleMemberData && singleMemberData.authorizedForms.length > 0 ? (
+              <p>
+                <span className="single-member-label">
+                  Authorized Weapons Forms:{" "}
+                </span>{" "}
+                {singleMemberData.authorizedForms.join(", ")}
+              </p>
+            ) : (
+              <p>
+                <span className="single-member-label">
+                  Authorized Weapons Forms:{" "}
+                </span>{" "}
+                No weapons forms authorized
+              </p>
+            )}
+
+            {singleMemberData ? (
+              <p>
+                <span className="single-member-label">
+                  Is Gear ready for fighting:{" "}
+                </span>{" "}
+                {singleMemberData.gearStatus ? (
+                  <span>Yes</span>
+                ) : (
+                  <span>no</span>
+                )}{" "}
+              </p>
+            ) : null}
+
+            {singleMemberData ? (
+              <p>
+                <span className="single-member-label">Member:</span>{" "}
+                {singleMemberData.memberStatus}
+              </p>
+            ) : null}
+
+            {singleMemberData ? (
+              <p>
+                <span className="single-member-label">Is Admin: </span>{" "}
+                {singleMemberData.admin ? <span>Yes</span> : <span>no</span>}{" "}
+              </p>
+            ) : null}
+
+            {singleMemberData ? (
+              <p>
+                <span className="single-member-label">Rank: </span>
+                {singleMemberData.rank}
+              </p>
+            ) : null}
+          </div>
+          {singleMemberData ? (
+            <img
+              src={singleMemberData.arms}
+              alt="the members arms"
+              className="single-member-image"
+            />
+          ) : null}
+
+          <button onClick={editClickHandler}>Edit</button>
+        </div>
       )}
-
-      {singleMemberData ? (
-        <p>
-          <span className="single-member-label">
-            Is Gear ready for fighting:{" "}
-          </span>{" "}
-          {singleMemberData.gearStatus ? <span>Yes</span> : <span>no</span>}{" "}
-        </p>
-      ) : null}
-
-      {singleMemberData ? (
-        <p>
-          <span className="single-member-label">Member:</span>{" "}
-          {singleMemberData.memberStatus}
-        </p>
-      ) : null}
-
-      {singleMemberData ? (
-        <p>
-          <span className="single-member-label">Is Admin: </span>{" "}
-          {singleMemberData.admin ? <span>Yes</span> : <span>no</span>}{" "}
-        </p>
-      ) : null}
-
-      {singleMemberData ? (
-        <p>
-          <span className="single-member-label">Rank: </span>
-          {singleMemberData.rank}
-        </p>
-      ) : null}
-
-      {singleMemberData ? (
-        <img
-          src={singleMemberData.arms}
-          alt="the members arms"
-          className="single-member-arms"
-        />
-      ) : null}
-    </div>
+    </>
   );
 };
 
