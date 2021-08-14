f822c545b4a5036771a6c4fe7d8eec8dca9a9234
Eddie Haddican <edwardhaddican@gmail.com>
2021-08-14 15:49:14 -0400

    add mongo atlas uri

--- server/db/index.js
+++ server/db/index.js
@@ -1,14 +1,13 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
+const uri =
+  process.env.NODE_ENV === "production"
+    ? process.env.MONGODB_URI
+    : "mongodb://localhost/carpathia";
 
-const db = mongoose.connect('mongodb://localhost/carpathia', {
+const db = mongoose.connect(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
-const conn = mongoose.connection
-conn.on('error', console.error.bind(console, "MongoDB connection error:"))
-
-
-
-
-
+const conn = mongoose.connection;
+conn.on("error", console.error.bind(console, "MongoDB connection error:"));
